98ececf6ef7c76a30fcc6cdde28dd0ef0cef89e2
Philip Crawford <philipharry@gmail.com>
2013-11-01 15:34:44 -0500

    Test for list of posts

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -8,4 +8,18 @@ feature 'Posts' do
     click_button 'Save Post'
     expect(page).to have_content('Rails is Awesome!')
   end
+
+  context 'when there are posts' do
+    before do
+      create :post, title: 'My first post'
+      create :post, title: 'My second post'
+    end
+
+    scenario 'can be listed' do
+      visit posts_path
+      expect(page).to have_content('My first post')
+      expect(page).to have_content('My second post')
+    end
+  end
+
 end
