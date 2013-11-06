862aee34efc389501f95df5a375dd4da4ec8dee6
Philip Crawford <philipharry@gmail.com>
2013-11-05 20:23:10 -0600

    Post has_many comments

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Post do
+  it { should have_many :comments }
+
   describe '#title' do
     it { expect(subject).to have_valid(:title).when 'X'*5 }
     it { expect(subject).to_not have_valid(:title).when nil, '', 'X'*4 }
