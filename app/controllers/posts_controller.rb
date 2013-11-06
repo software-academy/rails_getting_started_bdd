38cf9eddedcb07cbbf936061bbd2847716f70e38
Philip Crawford <philipharry@gmail.com>
2013-11-05 20:16:41 -0600

    Add link to destroy post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
